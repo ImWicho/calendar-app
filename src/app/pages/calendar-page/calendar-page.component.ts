73ae51256645f99f99e79194ce4d283ead42ea14
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2023-09-15 15:02:38 -0600

    [ADD] calendar and dashboard page

---
+++ src/app/pages/calendar-page/calendar-page.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+@Component({
+  selector: 'app-calendar-page',
+  standalone: true,
+  imports: [CommonModule, FullCalendarModule],
+  templateUrl: './calendar-page.component.html',
+  styleUrls: ['./calendar-page.component.scss']
+})
+export class CalendarPageComponent {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin],
+    height: '100%',
+    expandRows: true,
+    events: 'https://fullcalendar.io/api/demo-feeds/events.json?start=2023-09-10T00:00:00Z&end=2023-09-17T00:00:00Z',
+    headerToolbar: {
+      left: 'prev today',
+      center: 'title',
+      right: 'dayGridMonth,dayGridWeek,dayGridDay next'
+    },
+    eventTimeFormat : {
+      hour: 'numeric',
+      minute: '2-digit',
+      meridiem: false
+    },
+  };
+}
