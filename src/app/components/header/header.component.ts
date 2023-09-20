3cf554bd4ee508b124a8a5c57b4dd11db5f2c4a5
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2023-09-19 22:38:30 -0600

    [ADD] toggle theme was moved to a new component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,40 +1,14 @@
-import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DataService, THEME_KEY } from 'src/app/services/data.service';
+import { Component } from '@angular/core';
+import { ToggleThemeComponent } from '../toggle-theme/toggle-theme.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ToggleThemeComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  dataService = inject(DataService)
-  isDarkMode = false;
-  theme = 'light'
-  constructor(){
-    this.loadThemeState()
-  }
-  
-  toggleDarkMode(): void{
-    this.theme = this.theme === 'light' ? 'dark' : 'light'
-    this.setTheme()
-    this.dataService.setItem(THEME_KEY, this.theme)
-    this.isDarkMode = this.theme === 'dark'
-  }
 
-  setTheme():void{
-    const root: any = document.querySelector(':root')
-    root.setAttribute('data-color-mode', this.theme)
-  }
-
-  loadThemeState():void{
-    const theme = this.dataService.getItem(THEME_KEY)
-    if(theme){
-      this.theme = theme
-      this.isDarkMode = theme === 'dark'
-      this.setTheme()
-    }
-  }
 }
